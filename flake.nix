765cbea5dbbf723f18d1ff3163dfd2e1cc463fb2
brittonr <b@robitzs.ch>
2026-02-23 19:48:02 -0500

    Use rust-overlay with rust-src extension for hyperlight-wasm build

--- flake.nix
+++ flake.nix
@@ -4,20 +4,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.nightly."2026-02-06".default.override {
+          extensions = [ "rust-src" "llvm-tools" ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-            clippy
+          buildInputs = [
+            rustToolchain
           ];
 
           shellHook = ''
